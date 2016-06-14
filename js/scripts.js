6eafe0947890c1185cb1498e4d1b53dee26cbf01
Steven Tran and Yi Chiang <steven+yi@>
2016-06-14 14:16:58 -0700

    add toggle functionality to flashcards

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,5 @@
 $(document).ready(function() {
-  // $(".flashcard-question").click(function() {
-  //   $(".flashcard-answer").show();
-  // });
   $( ".flashcard-question" ).click(function() {
-      $( this ).show();
-      $( this ).children().show();
+      $( this ).children().toggleClass("show");
     });
 });
